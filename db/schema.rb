aae83bd7c7b0f7f61f41664740accf6189d5403c
mariusgg <marius.g.guei@gmail.com>
2018-12-02 01:43:01 +0000

    added comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_02_010341) do
+ActiveRecord::Schema.define(version: 2018_12_02_012737) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
